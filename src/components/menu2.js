de6a8b971fccf61c8ebbccde91b3d2b6629e4f67
Sultan Mahmud <sultanmahmud0@gmail.com>
2019-04-16 16:44:34 +0600

    Notification menu

--- src/components/menu2.js
+++ src/components/menu2.js
@@ -1,15 +1,39 @@
 import React, {Component} from 'react'; 
 
+
+
 class Menu2 extends Component{
+
+    constructor(){
+        super()
+        this.state = {
+            notificaion: [
+                "Notification-1",
+                "Notification-3",
+                "Notification-4",
+                "Notification-5",
+            ]
+        } 
+    }
+
+    getNotification = () =>{ 
+           return(
+            this.state.notificaion.map(items =>(
+                <li key={items}>{items}</li>
+            ))
+        ) 
+    }
+  
     render(){
+        
         return(
             <div className="header">
                 <div className="container">
                 <div className="row">
                         <div className="col-lg-12 col-sm-12 col-xs-12">
                             <div className="text-center mb-20">
-                                <h1>Menu 2</h1>
-                                <p>Menu 2 title</p>
+                                <h1>Notificaion Status</h1>
+                                <p>Check notificatin read/unread</p>
                             </div>
                         </div>
                     </div> 
@@ -17,12 +41,22 @@ class Menu2 extends Component{
                         <div className="col-lg-12 col-sm-12 col-xs-12">
                             <div className="card border-dark mb-3"> 
                                 <div className="card-body text-dark"> 
-                                    <p className="card-text">Pin Project Goes Here...</p> 
+                                    <p className="card-text" style={{textAlign: 'center'}}>
+
+                                        {this.state.notificaion.length > 0 
+                                         ?
+                                        <span>You Have <button onClick={this.getNotification}>{this.state.notificaion.length}</button> Unread Notifications</span>
+                                         : 
+                                        <span>You Have <button onClick={this.getNotification}>{this.state.notificaion.length}</button> Unread Notifications}</span>} 
+                                        
+                                    </p> 
+                                    <strong>{this.getNotification()}</strong>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                       
             </div>
         );
     }
