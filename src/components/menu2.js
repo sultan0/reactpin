a1f359b1fe339abf0b286725f2db27a606ee9d52
Sultan Mahmud <sultanmahmud0@gmail.com>
2019-04-18 16:03:52 +0600

    show Notifications

--- src/components/menu2.js
+++ src/components/menu2.js
@@ -12,16 +12,19 @@ class Menu2 extends Component{
                 "Notification-3",
                 "Notification-4",
                 "Notification-5",
-            ]
+            ],
+
+            showNotification: "",
+            visibility: false
         } 
     }
-
+ 
     getNotification = () =>{ 
-           return(
-            this.state.notificaion.map(items =>(
-                <li key={items}>{items}</li>
-            ))
-        ) 
+
+            this.setState({
+                showNotification: this.state.notificaion.map(items => (<li key={items}>{items}</li>
+                ))    
+            }) 
     }
   
     render(){
@@ -50,7 +53,9 @@ class Menu2 extends Component{
                                         <span>You Have <button onClick={this.getNotification}>{this.state.notificaion.length}</button> Unread Notifications}</span>} 
                                         
                                     </p> 
-                                    <strong>{this.getNotification()}</strong>
+                                    <strong>{this.state.showNotification}</strong>
+                                  
+                           
                                 </div>
                             </div>
                         </div>
